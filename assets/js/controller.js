b6c96d4786717dea540ba2b090e52449cd18b533
David Boschwitz <dbosch@iastate.edu>
2017-01-02 22:32:45 -0600

    set up open graph sharing stuffs

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,11 +1,11 @@
 angular.module('johnsApp', ['ngRoute', 'angular-loading-bar']) //ngRoute is an angular service
-//http://stackoverflow.com/questions/15449325/how-can-i-preserve-new-lines-in-an-angular-partial
-.filter('newline', function($sce) {
-    return function(text) {
-        text = text.replace(/\n/g, '<br />');
-        return $sce.trustAsHtml(text);
-    }
-})
+    //http://stackoverflow.com/questions/15449325/how-can-i-preserve-new-lines-in-an-angular-partial
+    .filter('newline', function($sce) {
+        return function(text) {
+            text = text.replace(/\n/g, '<br />');
+            return $sce.trustAsHtml(text);
+        }
+    })
     .config(function($routeProvider) {
         $routeProvider.when("/contact", {
             controller: "contactController",
@@ -33,61 +33,64 @@ angular.module('johnsApp', ['ngRoute', 'angular-loading-bar']) //ngRoute is an a
 
     })
 
-.service('Materialize', function() {
-    return Materialize;
-})
+    .service('Materialize', function() {
+        return Materialize;
+    })
 
-.factory('goto', function($location, $window, $timeout) {
-    return function(path) {
-        if (path.startsWith('/'))
-            path = path.substring(1);
-        $location.path(path);
-        $timeout(function() {
-            $window.$('ul.tabs').tabs();
-        }, 500);
-        window.scrollToID('#page-content');
-    };
-})
+    .factory('goto', function($location, $window, $timeout) {
+        return function(path) {
+            if (path.startsWith('/'))
+                path = path.substring(1);
+            $location.path(path);
+            $timeout(function() {
+                $window.$('ul.tabs').tabs();
+            }, 500);
+            window.scrollToID('#page-content');
+        };
+    })
 
-.controller('indexController', function($scope, $location, $route, goto) {
-    window.ugh = function() {
-        console.log($location, $route);
-    }
+    .controller('indexController', function($scope, $location, $route, goto) {
+        window.ugh = function() {
+            console.log($location, $route);
+        }
 
-    $scope.active = function(path) {
-        return $location.path() == path.split('/').join('') ? 'active' : '';
-    }
+        $scope.active = function(path) {
+            return $location.path() == path.split('/').join('') ? 'active' : '';
+        }
 
-    window.goto = $scope.goto = goto;
-})
+        window.goto = $scope.goto = goto;
+    })
 
-.controller('contactController', function($scope, goto) {
-  console.log('changed to contact page')
-})
+    .controller('contactController', function($scope, goto) {
+        console.log('changed to contact page')
+    })
 
-.controller('lyricsController', function($scope, $http) {
-  console.log('changed to lyrics page');
-$scope.selectedLyric = {text:''};
-  $http.get('https://vps.boschwitz.me/johns/backend/get.php?p=lyrics').then(function(res) {
-      console.log(res)
-      $scope.data = res.data;
-  });
+    .controller('lyricsController', function($scope, $http) {
+        console.log('changed to lyrics page');
+        $scope.selectedLyric = {
+            text: ''
+            ,title:'Please select a song'
+        };
+        $http.get('https://vps.boschwitz.me/johns/backend/get.php?p=lyrics').then(function(res) {
+            console.log(res)
+            $scope.data = res.data;
+        });
 
-  $scope.select = function(lyric){
-    $scope.selectedLyric = lyric;
-  }
-})
+        $scope.select = function(lyric) {
+            $scope.selectedLyric = lyric;
+        }
+    })
 
-.controller('showsController', function($scope, $http) {
-  console.log('changed to shows page');
-  $scope.data = {};
+    .controller('showsController', function($scope, $http) {
+        console.log('changed to shows page');
+        $scope.data = {};
 
-    $http.get('https://vps.boschwitz.me/johns/backend/get.php?p=shows').then(function(res) {
-        console.log(res)
-        $scope.data = res.data;
+        $http.get('https://vps.boschwitz.me/johns/backend/get.php?p=shows').then(function(res) {
+            console.log(res)
+            $scope.data = res.data;
+        });
+    })
+
+    .controller('musicController', function($scope, goto) {
+        console.log('changed to music page')
     });
-})
-
-.controller('musicController', function($scope, goto) {
-  console.log('changed to music page')
-});
